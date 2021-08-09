c2fa4876dfbd69170051f6a0fbb6fab259c8e6b1
Faisal Syarifuddin <faisal@ilcs.co.id>
2021-08-09 10:53:57 +0700

    login form (tinggal edit)
    Signed-off-by: Faisal Syarifuddin <faisal@ilcs.co.id>

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,12 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Beranda extends CI_Controller {
+class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('page/header_frontend');
-		$this->load->view('frontend/beranda');
+		$this->load->view('frontend/login');
 		$this->load->view('page/javascript_frontend');
 		$this->load->view('page/footer_frontend');
 	}
---
+++ application/views/frontend/login.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>SCBD IPDN</title>
+    <link href="https://upload.wikimedia.org/wikipedia/commons/5/56/Lambang_IPDN.png" rel="icon">
+    <link href="//fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/frontend/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/frontend/css/style-starter.css">
+</head>
+
+<style>
+</style>
+
+<body>
+    <!-- Login -->
+    <section class="w3l-contact py-5" id="contact">
+        <div class="container py-lg-5 py-md-4 py-2">
+            <div class="text-center mb-sm-5 mb-4">
+                <h3 class="title-big">Login</h3>
+            </div>
+            <div class="mx-auto pt-lg-4 pt-md-5 pt-4" style="max-width:1000px">
+                <div class="row contact-block">
+                    <div class="col-md-5 contact-left">
+                        <h3 class="font-weight-bold mb-md-5 mb-4">Contact Details</h3>
+                        <div class="cont-details">
+                            <div class="d-flex contact-grid">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-globe icon-color"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Company Address</h6>
+                                    <p>10001, 5th Avenue, 12202 street, USA.</p>
+                                </div>
+                            </div>
+                            <div class="d-flex contact-grid mt-4 pt-lg-2">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-phone icon-color"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Call Us</h6>
+                                    <p><a href="tel:+1(21) 234 4567">+1(21) 112 7368</a></p>
+                                </div>
+                            </div>
+                            <div class="d-flex contact-grid mt-4 pt-lg-2">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-envelope-open icon-color"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Email Us</h6>
+                                    <p><a href="mailto:example@mail.com" class="mail">example@mail.com</a></p>
+                                </div>
+                            </div>
+                            <div class="d-flex contact-grid mt-4 pt-lg-2">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-headphones icon-color"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Customer Support</h6>
+                                    <p><a href="mailto:info@support.com" class="mail">info@support.com</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7 contact-right mt-md-0 mt-4">
+                        <form action="https://sendmail.w3layouts.com/submitForm" method="post" class="signin-form">
+                            <div class="input-grids">
+                                <input type="text" name="w3lName" id="w3lName" placeholder="Your Name*"
+                                    class="contact-input" required="" />
+                                <input type="email" name="w3lSender" id="w3lSender" placeholder="Your Email*"
+                                    class="contact-input" required="" />
+                                <input type="text" name="w3lSubect" id="w3lSubect" placeholder="Subject*"
+                                    class="contact-input" required="" />
+                                <input type="text" name="w3lWebsite" id="w3lWebsite" placeholder="Website URL*"
+                                    class="contact-input" required="" />
+                            </div>
+                            <div class="form-input">
+                                <textarea name="w3lMessage" id="w3lMessage" placeholder="Type your message here*"
+                                    required=""></textarea>
+                            </div>
+                            <button class="btn button-style">Send Message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //Login -->
