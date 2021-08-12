c525f9fa65465e876cacbbeaaa3ed34340f80109
Faisal Syarifuddin <faisal@ilcs.co.id>
2021-08-12 16:28:06 +0700

    tentang
    Signed-off-by: Faisal Syarifuddin <faisal@ilcs.co.id>

--- application/views/frontend/tentang.php
+++ application/views/frontend/tentang.php
@@ -1,32 +1,48 @@
-    <!-- Banner -->
+    <!-- Tentang -->
     <section id="home" class="w3l-banner py-5">
-    <div class="banner-image">
-    </div>
-        <div class="banner-content">
-            <div class="container pt-5 pb-md-4">
-                <div class="row align-items-center">
-                    <div class="col-md-6 content-photo order-md-last order-first">
-                        <img src="assets/frontend/images/10586.png" class="img-fluid" alt="content-photo">
-                    </div>
-                    <div class="col-md-6 content-left mb-md-0 mb-5 pl-lg-5 order-md-first order-last">
-                        <h3 class="mb-3">Tentang Aplikasi</h3>
-                        <p class="mt-3" align="justify">
-                            <?php 
-                                if(!empty($desc)) {
-                                    foreach ($desc as $row) {
-                                        if($row->id_desc == 2) {
-                                            echo nl2br("&emsp; $row->desc <br>");
-                                            echo nl2br("&emsp; $row->sub_desc");
-                                        }
+        <div class="banner-image"></div>
+        <div class="container py-lg-5 py-md-3">
+            <div class="row">
+                <div class="col-lg-6 about-right-faq align-self pr-lg-5">
+                    <h3 class="title-big">Tentang Aplikasi</h3>
+                    <p class="mt-3 mb-4" align="justify">
+                        <?php 
+                            if(!empty($desc)) {
+                                foreach ($desc as $row) {
+                                    if($row->id_desc == 2) {
+                                        echo nl2br("$row->desc");
                                     }
-                                } else {
-                                    echo "";
                                 }
-                            ?>    
-                        </p>
-                    </div>
+                            } else {
+                                echo "";
+                            }
+                        ?>
+                    </p>
+                </div>
+                <div class="col-lg-6 left-wthree-img mt-lg-0 mt-5">
+                    <img src="assets/frontend/images/10586.png" alt="" class="img-fluid radius-image">
+                </div>
+            </div>
+            <div class="row mt-5 pt-5">
+                <div class="col-lg-6 left-wthree-img mt-lg-0 mt-5 order-lg-first order-last">
+                    <img src="assets/frontend/images/service2.png" alt="" class="img-fluid radius-image">
+                </div>
+                <div class="col-lg-6 about-right-faq align-self pl-lg-5 order-lg-last order-first">
+                    <p class="mt-3">
+                        <?php 
+                            if(!empty($desc)) {
+                                foreach ($desc as $row) {
+                                    if($row->id_desc == 2) {
+                                        echo nl2br("$row->sub_desc");
+                                    }
+                                }
+                            } else {
+                                echo "";
+                            }
+                        ?>
+                    </p>
                 </div>
             </div>
         </div>
     </section>
-    <!-- //Banner -->
+    <!-- //Tentang -->
