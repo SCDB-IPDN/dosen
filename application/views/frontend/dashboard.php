aab1267b2954a52f0b254b1c5df6067381366683
Faisal Syarifuddin <faisal@ilcs.co.id>
2021-08-26 15:36:24 +0700

    fixing dashboard dan variable not found
    Signed-off-by: Faisal Syarifuddin <faisal@ilcs.co.id>

--- application/views/frontend/dashboard.php
+++ application/views/frontend/dashboard.php
@@ -18,6 +18,9 @@ if ($get_absen_pulang_perbulan_chart != false && !empty($get_absen_pulang_perbul
     }
 } else {
     $count_pulang_perbulan = 0;
+    $count_pulang_perbulan1 = 0;
+    $count_pulang_perbulan2 = 0;
+    $count_pulang_perbulan3 = 0;
 }
 
 if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
@@ -39,6 +42,9 @@ if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan
     }
 } else {
     $count_masuk_perbulan = 0;
+    $count_masuk_perbulan1 = 0;
+    $count_masuk_perbulan2 = 0;
+    $count_masuk_perbulan3 = 0;
 }
 ?>
 
@@ -48,11 +54,11 @@ if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan
     </div>
 
     <div id="particles-js"></div>
-    <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script src="<?php echo base_url('assets/frontend/js/particles.min.js'); ?>"></script>
 
     <div class="banner-content">
         <div class="container pt-5 pb-md-4">
-            <div class="card border-primary text-center mb-3">
+            <div class="card border-primary text-center mb-3 mt-3">
                 <!-- <div class="card-header">
                     Dashboard Presensi Perbulan
                 </div> -->
@@ -61,8 +67,8 @@ if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan
                     <p class="card-text"><b>Total Data: <?= $count_pulang_perbulan; ?></b></p>
                 </div>
             </div>
-        <!-- </div> -->
-        <!-- <div class="container"> -->
+            <!-- </div> -->
+            <!-- <div class="container"> -->
             <div class="card-deck">
                 <div class="card border-primary">
                     <div class="card-body">
@@ -87,7 +93,7 @@ if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan
                 </div>
             </div>
         </div>
-    <!-- </div>
+        <!-- </div>
     <div class="banner-content"> -->
         <div class="container pt-5 pb-md-4">
             <div class="card border-primary text-center mb-3">
@@ -539,23 +545,22 @@ if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan
                 } ?>
             ],
             datasets: [{
-                    label: "Data Dosen Non PNS",
-                    fill: false,
-                    backgroundColor: 'rgb(60, 168, 58)',
-                    borderColor: 'rgb(0, 0, 0)',
-                    data: [
-                        <?php if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
-                            foreach ($get_absen_masuk_perbulan_chart as $data) {
-                                if ($data->role == "Dosen") {
-                                    echo $data->jumlah_hadir . ",";
-                                }
+                label: "Data Dosen Non PNS",
+                fill: false,
+                backgroundColor: 'rgb(60, 168, 58)',
+                borderColor: 'rgb(0, 0, 0)',
+                data: [
+                    <?php if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
+                        foreach ($get_absen_masuk_perbulan_chart as $data) {
+                            if ($data->role == "Dosen") {
+                                echo $data->jumlah_hadir . ",";
                             }
-                        } else {
-                            echo "0,";
-                        } ?>
-                    ]
-                }
-            ]
+                        }
+                    } else {
+                        echo "0,";
+                    } ?>
+                ]
+            }]
         },
 
         plugins: [ChartDataLabels],
@@ -584,108 +589,108 @@ if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan
         }
     });
 
-// ABSEN MASUK PNS PERBULAN
-var ctx = document.getElementById('absenmasukpnsperbln').getContext('2d');
-var absenmasukpnsperbln = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [
-            <?php if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
-                foreach ($get_absen_masuk_perbulan_chart as $data) {
-                    if ($data->role == "PNS") {
-                        switch ($data->bulan) {
-                            case "1":
-                                $namaBulan = "'Januari'";
-                                break;
-                            case "2":
-                                $namaBulan = "'Februari'";
-                                break;
-                            case "3":
-                                $namaBulan = "'Maret'";
-                                break;
-                            case "4":
-                                $namaBulan = "'April'";
-                                break;
-                            case "5":
-                                $namaBulan = "'Mei'";
-                                break;
-                            case "6":
-                                $namaBulan = "'Juni'";
-                                break;
-                            case "7":
-                                $namaBulan = "'Juli'";
-                                break;
-                            case "8":
-                                $namaBulan = "'Agustus'";
-                                break;
-                            case "9":
-                                $namaBulan = "'September'";
-                                break;
-                            case "10":
-                                $namaBulan = "'Oktober'";
-                                break;
-                            case "11":
-                                $namaBulan = "'November'";
-                                break;
-                            case "12":
-                                $namaBulan = "'Desember'";
-                                break;
-                            default:
-                                break;
-                        }
-                        echo $namaBulan . ",";
-                    }
-                }
-            } else {
-                echo "0,";
-            } ?>
-        ],
-        datasets: [{
-            label: "Data PNS",
-            fill: true,
-            backgroundColor: 'rgb(148, 80, 2)',
-            borderColor: 'rgb(0, 0, 0)',
-            data: [
-                <?php
-                if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
+    // ABSEN MASUK PNS PERBULAN
+    var ctx = document.getElementById('absenmasukpnsperbln').getContext('2d');
+    var absenmasukpnsperbln = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                <?php if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
                     foreach ($get_absen_masuk_perbulan_chart as $data) {
                         if ($data->role == "PNS") {
-                            echo $data->jumlah_hadir . ",";
+                            switch ($data->bulan) {
+                                case "1":
+                                    $namaBulan = "'Januari'";
+                                    break;
+                                case "2":
+                                    $namaBulan = "'Februari'";
+                                    break;
+                                case "3":
+                                    $namaBulan = "'Maret'";
+                                    break;
+                                case "4":
+                                    $namaBulan = "'April'";
+                                    break;
+                                case "5":
+                                    $namaBulan = "'Mei'";
+                                    break;
+                                case "6":
+                                    $namaBulan = "'Juni'";
+                                    break;
+                                case "7":
+                                    $namaBulan = "'Juli'";
+                                    break;
+                                case "8":
+                                    $namaBulan = "'Agustus'";
+                                    break;
+                                case "9":
+                                    $namaBulan = "'September'";
+                                    break;
+                                case "10":
+                                    $namaBulan = "'Oktober'";
+                                    break;
+                                case "11":
+                                    $namaBulan = "'November'";
+                                    break;
+                                case "12":
+                                    $namaBulan = "'Desember'";
+                                    break;
+                                default:
+                                    break;
+                            }
+                            echo $namaBulan . ",";
                         }
                     }
                 } else {
                     echo "0,";
                 } ?>
-            ]
-        }]
-    },
+            ],
+            datasets: [{
+                label: "Data PNS",
+                fill: true,
+                backgroundColor: 'rgb(148, 80, 2)',
+                borderColor: 'rgb(0, 0, 0)',
+                data: [
+                    <?php
+                    if ($get_absen_masuk_perbulan_chart != false && !empty($get_absen_masuk_perbulan_chart)) {
+                        foreach ($get_absen_masuk_perbulan_chart as $data) {
+                            if ($data->role == "PNS") {
+                                echo $data->jumlah_hadir . ",";
+                            }
+                        }
+                    } else {
+                        echo "0,";
+                    } ?>
+                ]
+            }]
+        },
 
-    plugins: [ChartDataLabels],
-    options: {
-        plugins: {
-            legend: {
-                display: false
-            },
-            datalabels: {
-                color: '#17202a',
-                font: {
-                    weight: 'bold',
-                }
-            },
-            title: {
-
-                font: {
-                    size: 20
+        plugins: [ChartDataLabels],
+        options: {
+            plugins: {
+                legend: {
+                    display: false
                 },
-                color: 'blue',
-                padding: {
-                    top: 10,
-                    bottom: 30
+                datalabels: {
+                    color: '#17202a',
+                    font: {
+                        weight: 'bold',
+                    }
+                },
+                title: {
+
+                    font: {
+                        size: 20
+                    },
+                    color: 'blue',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
                 }
             }
         }
-    }
-});
+    });
 
     // ABSEN MASUK PNS DOSEN PERBULAN
     var ctx = document.getElementById('absenmasukpnsdosenperbln').getContext('2d');
