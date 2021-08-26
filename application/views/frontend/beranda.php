df2f9828812e4898c9daad54a2923f5dcd924b32
Faisal Syarifuddin <faisal@ilcs.co.id>
2021-08-26 15:35:53 +0700

    fixin cdn block
    Signed-off-by: Faisal Syarifuddin <faisal@ilcs.co.id>

--- application/views/frontend/beranda.php
+++ application/views/frontend/beranda.php
@@ -4,7 +4,7 @@
         </div>
 
         <div id="particles-js"></div>
-        <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+        <script src="<?php echo base_url('assets/frontend/js/particles.min.js'); ?>"></script>
 
         <div class="banner-content">
             <div class="container pt-5 pb-md-4">
