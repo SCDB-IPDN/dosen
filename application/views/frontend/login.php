540be39cde47ee50d1bb3c1353c1a182b56c257e
Faisal Syarifuddin <faisal@ilcs.co.id>
2021-08-26 22:48:23 +0700

    revamp login form
    Signed-off-by: Faisal Syarifuddin <faisal@ilcs.co.id>

--- application/views/frontend/login.php
+++ application/views/frontend/login.php
@@ -1,104 +1,61 @@
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<!doctype html>
+<html lang="en">
 
-<style>
-    .container {
-        border: 0px solid black;
-        margin-top: 0;
-        padding-top: 20px;
-    }
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-    .title-big {
-        margin-bottom: 20px;
-    }
+    <link rel="stylesheet" href="<?php echo base_url('assets/login-form-07/fonts/icomoon/style.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/login-form-07/css/owl.carousel.min.css'); ?>">
 
-    .bg-doger {
-        background-color: dodgerblue;
-    }
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/login-form-07/css/bootstrap.min.css'); ?>">
+    <!-- Style -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/login-form-07/css/style.css'); ?>">
 
-    .bg-putih {
-        border-radius: 10px;
-        padding: 20px;
-        background-color: white;
-        box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    }
+    <title>PRESENSI IPDN &copy;<?php echo date('Y') ?></title>
+    <link href="https://upload.wikimedia.org/wikipedia/commons/5/56/Lambang_IPDN.png" rel="icon">
+</head>
 
-    .title-header {
-        font-size: 65px;
-        font-weight: bold;
-        letter-spacing: 3px;
-    }
+<body>
 
-    footer {
-        border-top: 1px solid gainsboro;
-        padding: 30px;
-    }
-
-    @media screen and(min-width:500px) {
-        .title-header {
-            font-size: 15px;
-        }
-
-        .bg-putih {
-            width: 95%;
-        }
-    }
-</style>
-
-<!-- Login -->
-<section id="home" class="w3l-banner py-5">
-    <div class="banner-image">
-    </div>
-
-    <div id="particles-js"></div>
-    <script src="<?php echo base_url('assets/frontend/js/particles.min.js'); ?>"></script>
-
-    <div class="container">
-        <center>
-            <h2 class="title-header" data-aos="fade-down">PRESENSI</h2>
-            <hr>
-        </center>
-        <div class="mx-auto pt-lg-4 pt-md-5 pt-4" style="max-width:1000px; margin-top:30px">
-            <div class="row contact-block">
-                <div class="col-md-5 contact-right mt-md-0 mt-4 bg-putih" data-aos="fade-left">
-                    <form action="<?= base_url('/ceklogin') ?>" method="post" class="signin-form">
-                        <h3 class="title-big">Login</h3>
-
-                        <div class="input-grids">
-                            <input type="text" name="username" id="w3lName" placeholder="Username*" class="contact-input inputan" required="" />
-                            <input type="password" name="password" id="w3lSender" placeholder="Password*" class="contact-input inputan" required="" />
-                        </div>
-                        <button type="submit" class="btn button-style bg-doger">LOGIN</button>
-
-                        <div style="margin: 20px 0; text-align:right;">
-                            <img style="display: block;" src="assets/frontend/images/smart-ipdn.png" width="40%">
-                        </div>
-                    </form>
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?php echo base_url('assets/login-form-07/images/undraw_remotely_2j6y.svg'); ?>" alt="Image" class="img-fluid">
                 </div>
-                <div class="col-md-7">
-                    <img data-aos="fade-right" src="assets/frontend/images/5584-2.png" width="100%">
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="mb-4">
+                                <h3>Login Form</h3>
+                                <p class="mb-4">Aplikasi Presensi</p>
+                            </div>
+                            <form action="<?= base_url('/ceklogin') ?>" method="post">
+                                <div class="form-group first">
+                                    <label for="username">Username</label>
+                                    <input type="text" class="form-control" id="username" name="username">
+                                </div>
+                                <div class="form-group last mb-4">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password">
+                                </div>
+                                <input type="submit" value="Log In" class="btn btn-block btn-primary">
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-</section>
-<!-- //Login -->
-
-<footer>
-    <center>
-        <p class="copy-text">&copy; <?php echo date('Y') ?> IPDN - All rights reserved.
-        </p>
-    </center>
-</footer>
-
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script>
-    AOS.init({
-        offset: 100, // offset (in px) from the original trigger point
-        delay: 1000, // values from 0 to 3000, with step 50ms
-        duration: 700, // values from 0 to 3000, with step 50ms
-    });
-</script>
+    <script src="<?php echo base_url('assets/login-form-07/js/jquery-3.3.1.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/login-form-07/js/popper.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/login-form-07/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/login-form-07/js/main.js'); ?>"></script>
 </body>
 
 </html>
