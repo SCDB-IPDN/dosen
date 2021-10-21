cf723ae5c4017ddcf5630cf13f7b533a746c110c
agungdeomartha <agungdeomartha09@gmail.com>
2021-10-21 15:28:24 +0700

    ubah password

--- application/models/Beranda_model.php
+++ application/models/Beranda_model.php
@@ -1,25 +1,29 @@
 <?php
 
-    class Beranda_model extends CI_Model {
-        
-	    public function __construct() {
-            $this->load->database();
-        }
+class Beranda_model extends CI_Model
+{
 
-        public function get_desc() {
-            $get_desc = $this->db
-                        ->select('*')
-                        ->from('tbl_desc')
-                        ->get();
-            if($get_desc->num_rows() > 0) {
-                return $get_desc->result();
-            } else {
-                return false;
-            }
-        }
+    public function __construct()
+    {
+        $this->load->database();
+    }
 
-        public function get_profile($username) {
-            $get_user = $this->db->query("SELECT
+    public function get_desc()
+    {
+        $get_desc = $this->db
+            ->select('*')
+            ->from('tbl_desc')
+            ->get();
+        if ($get_desc->num_rows() > 0) {
+            return $get_desc->result();
+        } else {
+            return false;
+        }
+    }
+
+    public function get_profile($username)
+    {
+        $get_user = $this->db->query("SELECT
                             -- tbl_login.username,
                             -- tbl_login.PASSWORD,
                             tbl_login.image_url,
@@ -53,7 +57,22 @@
                             JOIN tbl_ta ON tbl_login.username = tbl_ta.nik 
                         WHERE
                             tbl_login.username = '$username'")->result();
-            return $get_user;
+        return $get_user;
+    }
+
+    public function edit_password($id)
+    {
+        $this->db->select("*");
+        $this->db->from("tbl_login");
+        $this->db->where("username", $id);
+        $query = $this->db->get();
+        if (count($query->result()) > 0) {
+            return $query->row();
         }
     }
-?>
+
+    public function update_password($data)
+    {
+        return $this->db->update('tbl_login', $data, array('username' => $data['username']));
+    }
+}
--- application/views/frontend/profile.php
+++ application/views/frontend/profile.php
@@ -56,6 +56,11 @@
                                         <p><?= $this->session->userdata('password'); ?></p>
                                     </div>
                                 </div>
+                                <div class="d-flex contact-grid mt-4 pt-lg-2">
+                                    <div class="cont-right">
+                                        <a class="btn button-style my-1 mx-auto text-light" value="<?= $this->session->userdata('username'); ?>" id="edit" role="button">Ubah Password</a>
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
@@ -63,5 +68,108 @@
                 </div>
             </div>
         </div>
+
+        <!-- Edit Record Modal -->
+        <div class="modal fade" id="edit_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ubah Password</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Edit Record Form -->
+                        <form action="" method="post" id="edit_form">
+                            <input type="hidden" id="edit_record_id" name="edit_record_id" value="">
+                            <div class="form-group">
+                                <label for="">NIP</label>
+                                <input disabled type="text" id="edit_nip" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password Baru</label>
+                                <input type="password" id="edit_password" class="form-control">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="update">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </section>
+
+    <input type="hidden" value="<?php echo base_url(); ?>" id="base_url">
+    <?php $this->load->view('page/js_datatable_frontend'); ?>
+
+    <script>
+        // Edit Record
+
+        $(document).on("click", "#edit", function(e) {
+            e.preventDefault();
+
+            var edit_id = $(this).attr("value");
+
+            $.ajax({
+                url: "<?php echo base_url(); ?>Beranda/edit",
+                type: "post",
+                dataType: "json",
+                data: {
+                    edit_id: edit_id
+                },
+                success: function(data) {
+                    if (data.responce == "success") {
+                        $('#edit_modal').modal('show');
+                        $("#edit_record_id").val(data.post.username);
+                        $("#edit_nip").val(data.post.username);
+                        $("#edit_password").val(data.post.password);
+                    } else {
+                        toastr["error"](data.message);
+                    }
+                }
+            });
+
+        });
+
+        // Update Record
+
+        $(document).on("click", "#update", function(e) {
+            e.preventDefault();
+
+            var edit_record_id = $("#edit_record_id").val();
+            var edit_nip = $("#edit_nip").val();
+            var edit_password = $("#edit_password").val();
+
+            if (edit_record_id == "" || edit_nip == "" || edit_password == "") {
+                alert("Both field is required");
+            } else {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>Beranda/update",
+                    type: "post",
+                    dataType: "json",
+                    data: {
+                        edit_record_id: edit_record_id,
+                        edit_nip: edit_nip,
+                        edit_password: edit_password
+                    },
+                    success: function(data) {
+                        if (data.responce == "success") {
+                            // $('#records').DataTable().destroy();
+                            // fetch();
+                            $('#edit_modal').modal('hide');
+                            toastr["success"](data.message);
+                        } else {
+                            toastr["error"](data.message);
+                        }
+                    }
+                });
+
+            }
+
+        });
+    </script>
     <!-- //Profile -->
