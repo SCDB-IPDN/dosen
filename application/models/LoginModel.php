3763c3fa0ef013f77c5320b8da74b817c7cf4a2b
ichaltkj96@gmail.com <ichaltkj96@gmail.com>
2021-11-10 17:03:11 +0700

    tambah akses role kepegawaian
    Signed-off-by: ichaltkj96@gmail.com <ichaltkj96@gmail.com>

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -13,7 +13,7 @@ class LoginModel extends CI_Model
                     ->select('id')
                     ->from($tabel)
                     ->where($kriteria)
-                    ->where_in('role',[1,22,23,29,30,31,32,33,34,35,36,37,38,39,40])
+                    ->where_in('role',[1,24,22,23,29,30,31,32,33,34,35,36,37,38,39,40])
                     ->get();
         if($cekLogin->num_rows() > 0) {
             return $cekLogin->num_rows();
